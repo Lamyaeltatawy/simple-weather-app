3dee5217c237966ce8f75c8b898888ddde927654
Lamyaeltatawy <101181841+Lamyaeltatawy@users.noreply.github.com>
2022-08-07 11:40:36 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const head=document.querySelector('.container');
+console.dir(head);
+head.style.color='#3973ac';
+head.style.background='#cce6ff';
+head.style.textAlign = 'center';
+head.style.fontSize = '40PX';
+head.style.border = "1px solid #80c1ff";
+/*head.style.width = "1000px";*/
+head.style.alignItems = "center";
+const search=document.querySelector('input[name="city"]');
+search.setAttribute('placeholder','Search For A City');
+
+
+const btn=document.querySelector('button');

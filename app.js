88186e58cbedf2cd8105518757cd9a9047995f55
Lamyaeltatawy <101181841+Lamyaeltatawy@users.noreply.github.com>
2022-08-07 16:56:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,5 +10,33 @@ head.style.alignItems = "center";
 const search=document.querySelector('input[name="city"]');
 search.setAttribute('placeholder','Search For A City');
 
+/*cont button=document.querySelector('button');*/
+let weather ={
+"apikey=":"8797b476b47b78b2c3f4035a9e619c5e"
+fetchWeather:function(){
+fetch(http://api.openweathermap.org/data/2.5/weather%20q=" + town + "app%20id=%208797b476b47b78b2c3f4035a9e619c5e
+).then((response)=>response.json())
+.then((data)=>console.log(data));
+}
 
-const btn=document.querySelector('button');
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
